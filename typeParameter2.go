c0148db4690c4211884a3eeeb2aec2dff13b490f
Faizan Ahmad <faizanahmad1033@gmail.com>
2022-07-18 16:42:56 +0500

    Type parameter 2nd commit

--- typeParameter2.go
+++ typeParameter2.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func Any[T any](s []T) {
-	for _, v := range s {
-		fmt.Println(v)
-	}
-}
 func main() {
-	s := []int{1, 23, 3, 34, 55}
-	ss := []string{"Hello", "World", "How", "Are", "You"}
-	Any(s)
-	Any(ss)
+	// Initializing the Strings
+	r := "Faizaaan"
+
+	// Display the Strings
+	fmt.Println("Given String: \n", r)
+
+	// Using the Replace Function
+	testresults := strings.Replace(r, "a", "", 3)
+
+	// Display the ReplaceAll Output
+	fmt.Println("\n After Replacing: \n", testresults)
+
 }
