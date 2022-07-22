09e8dcf9d67ed691312194eddf81a35c7ec12631
Faizan Ahmad <faizanahmad1033@gmail.com>
2022-07-22 17:27:36 +0500

    modified file

--- Goroutines.go
+++ Goroutines.go
@@ -10,6 +10,9 @@ func main() {
 }
 
 //to create a go routine we use the keyword go
+
+//when we are using go function it only prints the non go function the reason is that
+//you did not mentioned when to come on go function
 func greeter(s string) {
 	for i := 0; i < 5; i++ {
 		fmt.Println(s)
